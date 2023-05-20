201dbc3a3b3a4f386647eb9d57ab3f2054d55bcf
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-05-21 01:44:04 +0300

    update ImageGalleryItem with hook

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-export default class ImageGalleryItem extends Component {
-  static defaultProps = {
-    src: '',
-    alt: '',
-    largeImage: '',
+export default function ImageGalleryItem({
+  src = '',
+  alt = '',
+  largeImage = '',
+  isShowModal,
+}) {
+  const createModal = () => {
+    isShowModal(largeImage, alt);
   };
 
-  createModal = () => {
-    const { largeImage, alt } = this.props;
-    this.props.isShowModal(largeImage, alt);
-  };
-
-  render() {
-    const { src, alt } = this.props;
-
-    return (
-      <li className={css.imageGalleryItem}>
-        <img src={src} alt={alt} onClick={this.createModal} />
-      </li>
-    );
-  }
+  return (
+    <li className={css.imageGalleryItem}>
+      <img src={src} alt={alt} onClick={createModal} />
+    </li>
+  );
 }
 
 ImageGalleryItem.propTypes = {
